56c9d65c11de8dbfcede680510257287492fe0d8
thoffart <thiagohoffart321920@gmail.com>
2020-06-27 18:16:42 -0300

    feat(example): Updating example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,44 +6,22 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Customizable MultiselectField'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -51,10 +29,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -65,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CustomizableMultiselectField(
-                dataSource: [
+                dataSourceList: [
                   DataSource<String>(
                     dataList: [
                       {
@@ -93,8 +69,16 @@ class _MyHomePageState extends State<MyHomePage> {
                         'value': 'test6',
                       },
                     ],
-                    options: DataSourceOptions(valueKey: 'value', labelKey: 'label', title: 'label1'),
-                    valueList: ['test1', 'test2', 'test3', 'test4', 'test5', 'test6'],
+                    options: DataSourceOptions(
+                        valueKey: 'value', labelKey: 'label', title: 'label1'),
+                    valueList: [
+                      'test1',
+                      'test2',
+                      'test3',
+                      'test4',
+                      'test5',
+                      'test6'
+                    ],
                   ),
                   DataSource(
                     dataList: [
@@ -111,7 +95,8 @@ class _MyHomePageState extends State<MyHomePage> {
                         'value': 3,
                       },
                     ],
-                    options: DataSourceOptions(valueKey: 'value', labelKey: 'number', title: 'label2'),
+                    options: DataSourceOptions(
+                        valueKey: 'value', labelKey: 'number', title: 'label2'),
                     valueList: [1, 2, 3],
                   ),
                   DataSource<int>(
@@ -129,7 +114,10 @@ class _MyHomePageState extends State<MyHomePage> {
                         'value': 3,
                       },
                     ],
-                    options: DataSourceOptions(valueKey: 'value', labelKey: 'number', title: 'number3'),
+                    options: DataSourceOptions(
+                        valueKey: 'value',
+                        labelKey: 'number',
+                        title: 'number3'),
                     valueList: [],
                   ),
                   DataSource<String>(
@@ -144,7 +132,8 @@ class _MyHomePageState extends State<MyHomePage> {
                         'key': 'key3',
                       },
                     ],
-                    options: DataSourceOptions(valueKey: 'key', labelKey: 'key', title: 'number4'),
+                    options: DataSourceOptions(
+                        valueKey: 'key', labelKey: 'key', title: 'number4'),
                     valueList: ['key1'],
                   ),
                 ],
