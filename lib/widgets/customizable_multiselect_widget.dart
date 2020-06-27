15ba11f4c6bd1829074c20c572de4f22f336e144
thoffart <thiagohoffart321920@gmail.com>
2020-06-27 18:16:19 -0300

    feat(CustomizableMultiselectWidget): Updating CustomizableMultiselectWidget

--- lib/widgets/customizable_multiselect_widget.dart
+++ lib/widgets/customizable_multiselect_widget.dart
@@ -1,140 +1,109 @@
+import 'package:customizable_multiselect_flutter/models/customizable_multiselect_dialog_options.dart';
+import 'package:customizable_multiselect_flutter/models/customizable_multiselect_widget_options.dart';
 import 'package:customizable_multiselect_flutter/models/data_source.dart';
+import 'package:customizable_multiselect_flutter/widgets/customizable_multiselect_dialog.dart';
 import 'package:flutter/material.dart';
 
 class CustomizableMultiselectWidget extends StatefulWidget {
 
   const CustomizableMultiselectWidget({
     Key key,
-    @required this.dataSource,
-    this.enable,
-    this.enableSearchBar = false,
-    this.decoration,
-    this.chipColor,
-    this.chipShape,
-    this.hintText,
-    this.dialogTitle,
+    @required this.dataSourceList,
     this.onChanged,
     this.onFieldSubmitted,
+    @required this.customizableMultiselectDialogOptions,
+    @required this.customizableMultiselectWidgetOptions,
   }) : super(key: key);
 
-/*   /// Title for the AlertDialog
-  final String titleText;
-  final String hintText;
-  final bool required;
-  final String errorText;
-  final List<List> dataSource;
-  final String textField;
-  final String valueField;
-  final Function change;
-  final Function open;
-  final Function close;
-  final Widget leading;
-  final Widget trailing;
-  final String okButtonLabel;
-  final String cancelButtonLabel;
-  final Color fillColor;
-  final InputBorder border; */
-
-  final List<DataSource> dataSource;
-  final bool enable;
-  final bool enableSearchBar;
-  final InputDecoration decoration;
-  final Color chipColor;
-  final ShapeBorder chipShape;
-  final Text hintText;
-  final Text dialogTitle;
+  final List<DataSource> dataSourceList;
   final ValueChanged<List<List<dynamic>>> onChanged;
   final ValueChanged<List<List<dynamic>>> onFieldSubmitted;
+  final CustomizableMultiselectDialogOptions customizableMultiselectDialogOptions;
+  final CustomizableMultiselectWidgetOptions customizableMultiselectWidgetOptions;
 
   @override
   _CustomizableMultiselectWidgetState createState() => _CustomizableMultiselectWidgetState();
+
 }
 
 class _CustomizableMultiselectWidgetState extends State<CustomizableMultiselectWidget> {
 
-
-  List<Widget> _buildListChip(DataSource dataSource) => dataSource.valueList
-    .map((value) => dataSource.dataList.singleWhere((data) => data[dataSource.options.valueKey] == value, orElse: () => null))
-    .map((value) =>
-      (value != null) 
+  List<Widget> _buildListChip(DataSource dataSourceList) => dataSourceList.valueList
+    .map((value) => dataSourceList.dataList.singleWhere((data) => data[dataSourceList.options.valueKey] == value,orElse: () => null))
+    .map((value) => (value != null)
       ? Chip(
-          label: Text(value[dataSource.options.labelKey].toString(), overflow: TextOverflow.ellipsis),
-          backgroundColor: widget.chipColor ?? Colors.white,
-          shape:  widget.chipShape ?? RoundedRectangleBorder(
-            side: BorderSide(color: Colors.blue, width: 1),
-            borderRadius: BorderRadius.circular(40.0),
-          ),
-        )
-      : Container()
-  ).toList();
-  
+        label: Text(value[dataSourceList.options.labelKey].toString(), overflow: TextOverflow.ellipsis),
+        backgroundColor: widget.customizableMultiselectWidgetOptions.chipColor,
+        shape: widget.customizableMultiselectWidgetOptions.chipShape ?? RoundedRectangleBorder(
+          side: BorderSide(color: Colors.blue, width: 1),
+          borderRadius: BorderRadius.circular(40.0),
+        ),
+      )
+    : Container())
+  .toList();
+
   bool _checkIfAllDataSourceIsEmpty() {
-    return widget.dataSource.every((element) => element.dataList.isEmpty);
+    return widget.dataSourceList
+        .every((DataSource element) => element.dataList.isEmpty);
   }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () => {
-        if(!widget.enable) {
-
+      onTap: () async => {
+        if(widget.customizableMultiselectWidgetOptions.enable) {
+          await showDialog<List>(
+            context: context,
+            builder: (BuildContext context) {
+              return CustomizableMultiselectDialog(
+                dataSourceList: widget.dataSourceList,
+                customizableMultiselectDialogOptions: widget.customizableMultiselectDialogOptions,
+              );
+            },
+          )
         }
       },
       child: InputDecorator(
-        decoration: widget.decoration ?? InputDecoration(
-          filled: false,
-          errorMaxLines: 4,
-        ),
-        child:
-        (!_checkIfAllDataSourceIsEmpty())
-        ? Column(
-          children: <Widget>[
-            ...widget.dataSource.map((DataSource dataSource) => 
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    if(dataSource.options.title != null)
-                      Row(
+        decoration: widget.customizableMultiselectWidgetOptions.decoration,
+        child: (!_checkIfAllDataSourceIsEmpty())
+            ? Column(
+                children: <Widget>[
+                  ...widget.dataSourceList.map(
+                    (DataSource dataSourceList) => (dataSourceList.valueList.isNotEmpty)
+                    ? Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
                         children: [
-                          Expanded(child: Text(dataSource.options.title, textAlign: TextAlign.start)),
-                        ],
-                      ),
-                    SizedBox(height: 4),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
-                      child: Row(
-                        children: <Widget>[
-                          dataSource.valueList.isNotEmpty
-                          ? Expanded(
-                            child: Wrap(
-                              spacing: 8.0,
-                              children: _buildListChip(dataSource),
-                            )
-                          )
-                          : Expanded(
-                            child: Container(
-                              padding: EdgeInsets.only(top: 4, left: 12),
-                              child: Text(
-                                'Sem valores',
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold
-                                ),
-                              ),
+                          if (dataSourceList.options.title != null)
+                            Row(
+                              children: [
+                                Expanded(
+                                    child: Text(dataSourceList.options.title,
+                                        textAlign: TextAlign.start)),
+                              ],
+                            ),
+                          SizedBox(height: 4),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
+                            child: Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Wrap(
+                                  spacing: 8.0,
+                                  children:
+                                      _buildListChip(dataSourceList),
+                                ))
+                              ],
                             ),
                           ),
                         ],
                       ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
-        )
-        : Text('vazio'),
+                    )
+                    : Container(),
+                  )
+                ],
+              )
+            : widget.customizableMultiselectWidgetOptions.hintText,
       ),
     );
   }
