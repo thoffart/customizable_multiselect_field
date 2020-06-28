1a503065448a89e5f7bebb82bbd18742630c4145
thoffart <thiagohoffart321920@gmail.com>
2020-06-28 02:15:00 -0300

    feat(pubspec): Adding homePage

--- lib/customizable_multiselect_flutter.dart
+++ lib/customizable_multiselect_flutter.dart
@@ -1,7 +1,7 @@
 library customizable_multiselect_field;
 
-export './widgets/customizable_multiselect_field.dart';
-export './models/data_source.dart';
-export './models/data_source_options.dart';
-export './models/customizable_multiselect_dialog_options.dart';
-export './models/customizable_multiselect_widget_options.dart';
+export 'models/data_source.dart';
+export 'models/data_source_options.dart';
+export 'models/customizable_multiselect_dialog_options.dart';
+export 'models/customizable_multiselect_widget_options.dart';
+export 'widgets/customizable_multiselect_Field.dart';
